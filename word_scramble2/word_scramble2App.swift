c63b8cb5336849f00fe8fec9ae8eb9045ff8b7fe
CharlesMich <charleedongo@hotmail.com>
2025-01-10 08:30:49 -0600

    Initial Commit

---
+++ word_scramble2/word_scramble2App.swift
@@ -0,0 +1,17 @@
+//
+//  word_scramble2App.swift
+//  word_scramble2
+//
+//  Created by Charles Michael on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct word_scramble2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
